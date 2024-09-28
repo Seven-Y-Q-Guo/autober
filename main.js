8b4ccab3663cbe2d79d435dc4d71b21a9d9d8f0b
Seven Y Q Guo <superShy77@163.com>
2024-09-28 17:23:28 +0800

    simple scripts for baidu

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('test');
+const steps = [{
+  selector: "#kw",
+  value: "世界上最高的山峰是什么",
+  type: "input"
+}, {
+  selector: "#su",
+  type: "button"
+}];
+
+function run(steps) {
+  steps.forEach(({ type, selector, value }) => {
+    switch (type) {
+      case "input":
+        document.querySelector(selector).value = value;
+        break;
+      case "button":
+        document.querySelector(selector).click();
+        break;
+      default:
+        console.log("default");
+    }
+  });
+}
+
+run(steps);
